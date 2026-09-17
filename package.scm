bfd54bc4443a5fe6b73f10660988f676fbe98970
max <mxnmn.schndr@gmail.com>
2026-09-17 20:26:30 +0200

    guix

--- package.scm
+++ package.scm
@@ -1,8 +1,12 @@
+(use-modules (guix profiles)
+             (gnu packages))
+
 (specifications->manifest
   '(
     "river"
     "zig"
     "wayland"
+    "wayland:bin"       
     "wayland-protocols"
     "libxkbcommon"
     "pkg-config"
